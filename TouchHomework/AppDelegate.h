399492998126d487c88636ad61c38c45d06d3082
riko105 <stepanpaholyk@gmail.com>
2016-08-16 08:53:39 +0300

    Initial Commit

---
+++ TouchHomework/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchHomework
+//
+//  Created by Stepan Paholyk on 8/16/16.
+//  Copyright © 2016 Stepan Paholyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TouchHomework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TouchHomework
+//
+//  Created by Stepan Paholyk on 8/16/16.
+//  Copyright © 2016 Stepan Paholyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
